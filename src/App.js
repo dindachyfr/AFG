aafabefbe0684326643bac6334a5eedb2a13ee65
dindachyfr <dindachyfr@gmail.com>
2022-04-05 13:34:17 +0800

    add router guard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
 import Login from './pages/Login'
+import PublicRoute from './RouterGuard/PublicRoute'
+import PrivateRoute from './RouterGuard/PrivateRoute'
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="login"
-          element={<Login />}
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>}
         />
-        <Route path="/" 
-        element={<Home />}
+        <Route path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>}
         />
 
       </Routes>
